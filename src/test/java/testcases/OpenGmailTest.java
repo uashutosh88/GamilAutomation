8e97b77ec64c601a4ea29cab58f0780ed3350951
Ashutosh <uashutoshu@gmail.com>
2017-11-26 00:49:45 +0530

    comitting the changes made

--- src/test/java/testcases/OpenGmailTest.java
+++ src/test/java/testcases/OpenGmailTest.java
@@ -9,6 +9,7 @@ public class OpenGmailTest extends TestBase {
 	@Test
 	public void openGmailTest() {
 		click("Gmail_xpath");
+		System.out.println("login done");
 		Assert.assertTrue(isElementPresent("Login_xpath"), "Login not successful");
 		//Assert.fail("Bank Manager login not successful");
 	}
